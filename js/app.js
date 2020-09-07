b5f0f716b78efda3963a225eec27f853e8ed3fc1
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-09-07 17:04:18 +0300

    after adding 5 file css/ style.css,reset.css  ,js app.js  and index.html

---
+++ js/app.js
@@ -0,0 +1,54 @@
+'use strict';
+var name= prompt(" Enter Your Name ,Please!");
+var greeting=prompt('How Are You Today ? ');
+alert(' Greeting ' + name + ' Wish To You A Nice Day and Stay Safe ');
+ var answer = confirm('My name is Hadeel?');
+ if(answer==true){
+    console.log('YEEEEEEEES');
+    alert("true")
+}
+ var mymajor= prompt(" Guess what is  my major   ( Software engineering ,computer science and computer engineering? )");
+ if(mymajor.toLowerCase()=='software engineering'){
+    alert(' Youre great');
+}
+
+ var myage=prompt(" Guess my age :) hint **  \' i was born in 1998 \'  ");
+ if( parseInt(myage) ==22){
+    alert(' Youre Good in guess :) ');}
+ var fav=prompt(' Guess what i love more (front, back) hint ** \' i like the  desin \'');
+ 
+ if(fav.toLowerCase()=="front"){
+    alert('  GOOD  JOB :) ');
+
+ }
+
+var cat=confirm(" I don't like cat?");
+if(cat==true){
+    console.log('True');
+    alert("TRUEEEEEEE")
+}
+
+var eat=confirm("I don't eat mansaf ?");
+if(eat==true){
+    console.log('False , does anyone hate a  mansaf?');
+    alert("False , does anyone hate a  mansaf?")
+}
+
+var sleep=confirm(" I don't sleep over than 7 hours daily?  ");
+if(sleep==true){
+    console.log('True');
+    alert("TRUEEEEEEE")
+}
+
+
+var dif=confirm(" I have a different twines? ")
+if(dif==true){
+    console.log('True');
+    alert("TRUEEEEEEE")
+}
+
+ 
+
+ 
+ 
+
