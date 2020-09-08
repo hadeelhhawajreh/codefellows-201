ff70ca17fd69c6b651c388a603d108cd50451d6b
Hadeel H Al-Hawajreh <70091170+hadeelhhawajreh@users.noreply.github.com>
2020-09-08 15:08:08 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,37 +1,53 @@
 'use strict';
 //to enter user name
+
 var name= prompt(" Enter Your Name ,please !");
-//greeting
+//greeting the user 
+
 var greeting=prompt('How Are You Today ? ');
 // greeing alert 
 alert("Hey, " +name + ", have a nice day and stay safe . \n  Greetings "  );
-// yes/no question about my name
+//  prompt to guess   my name
  var answer = prompt("My name is Hadeel? \n  Yes/No ,and you can put y/n also").toLowerCase();
  if(answer=='yes' ||answer=='y' ){
     console.log('YEEEEEEEES, You   get it right');
     alert("True , my name is hadeel hussam .")
 }
+
+else if(answer=='no' ||answer=='n')
+{
+
+    alert('You didnt  get it right.');
+}
 else{
-    alert('You didnt  get it right.')
+    alert('You entered  invalid value');
 }
 // promot to guess my major 
 var mymajor= prompt("My Major is  Software engineering ? \n Yes/No  ,and you can put y/n also  ").toLowerCase();
  if(mymajor=='yes'||mymajor=='y'){
     // alert appers when the user click true
     console.log('YEEEEEEEES, great ');
-
     alert(' Yes true , i am  a senior software engineering  ');
 }
+else if (mymajor=='no'||mymajor=='n')
+{
+    alert('You didnt  get it right.');
+}
 else{
-    alert('You didnt  get it right.')
+    alert('You entered  invalid value');
 }
 // promote to guess my age
  var myage=prompt(" Is my age in 22 ?  \n Yes/No  ,and you can put y/n also  ").toLowerCase();
-if(myage=='yes'||myage=='y'){ // alert appers when the user click true
+if(myage=='yes'||myage=='y'){
+     // alert appers when the user click true
     alert('  Yes true ,  i was born in 1998 :) ');
 }
+else if(myage=='no'||myage=='n')
+{
+    alert('You didnt  get it right.');
+}
 else{
-    alert('You didnt  get it right.')
+    alert('You entered  invalid value');
 }
 
 var cat=prompt(" I don't like cat?").toLowerCase();
@@ -40,8 +56,12 @@ if(cat=='yes' || cat=='y'){
     console.log('True');
     alert("I'm really afraid of cats.");
 }
+else if(cat=='no' || cat=='n')
+{
+    alert('You didnt  get it right.');
+}
 else{
-    alert('You didnt  get it right.')
+    alert('You entered  invalid value');
 }
 
 var eat=prompt("I don't eat mansaf ? \n Yes/No  ,and you can put y/n also ").toLowerCase();
@@ -50,8 +70,12 @@ if(eat=='no'||eat=='n'){
     console.log(' does anyone hate a  mansaf?');
     alert(" does anyone hate a  mansaf?");
 }
+else if(eat=='yes'||eat=='y')
+{
+    alert('You didnt  get it right.');
+}
 else{
-    alert('You didnt  get it right.')
+    alert('You entered  invalid value');
 }
 
 var dif=prompt(" I have a different twines?   \n Yes/No  ,and you can put y/n also  ",'yes').toLowerCase();
@@ -61,6 +85,10 @@ if(dif=='yes'|| dif=='y'){
     console.log('True');
     alert("Yah ,I have a twin brother.");
 }
+else if(dif=='no'|| dif=='n')
+{
+    alert('You didnt  get it right.');
+}
 else{
-    alert('You didnt  get it right.')
+    alert('You entered  invalid value');
 }
